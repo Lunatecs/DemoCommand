3a361bbba3c0b188705ac7be03af1ae1a8fc30bc
wpohlhau <william.pohlhaus@villanova.edu>
2019-02-02 15:52:33 -0500

    Added examples in RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -23,4 +23,8 @@ public class RobotMap {
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
   // public static int rangefinderModule = 1;
+
+  public static int LEFT_TALON_CAN_ID = 3;
+  public static int LEFT_VICTOR_CAN_ID = 2;
+
 }

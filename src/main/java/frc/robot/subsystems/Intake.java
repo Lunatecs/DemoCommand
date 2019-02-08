bb5d31632814a54941f96cce1cdc67c973af4314
baxtrax <bradofficialbox@gmail.com>
2019-02-07 19:23:03 -0500

    Added setSpeed and getAngle for Intake subsystem

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,8 +8,12 @@
 package frc.robot.subsystems;
 
 import frc.robot.RobotMap;
-import edu.wpi.first.wpilibj.command.Subsystem;
 
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
@@ -17,13 +21,23 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 public class Intake extends Subsystem {
 
   private TalonSRX intakePivot = new TalonSRX(RobotMap.INTAKE_TALON_CAN_ID);
+  private AnalogInput ai = new AnalogInput(3);
+  private AnalogPotentiometer potentiometer = new AnalogPotentiometer(ai, 1080, 30);
+  private double angle = potentiometer.get();
   
   //constructor
-  public Intake(){
+  public Intake() {
     //No need for constant
     intakePivot.setNeutralMode(NeutralMode.Brake);
   }
 
+  public void setSpeed(double speed) {
+    intakePivot.set(ControlMode.PercentOutput, speed); 
+  }
+
+  public double getAngle() { 
+    return potentiometer.get();
+  }
 
   @Override
   public void initDefaultCommand() {
